bc0a0ecac50993285053a9a5dc66a80ef157e2c2
jules <1978643+julesferreira@users.noreply.github.com>
2021-02-28 13:25:49 -0700

    set chezmoi bin dir

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@
 set -e # -e: exit on error
 
 if [ ! "$(command -v chezmoi)" ]; then
-  chezmoi="chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+	bin_dir="$HOME/.local/bin"
+	chezmoi="$bin_dir/chezmoi"
+	if [ "$(command -v curl)" ]; then
+		sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+	elif [ "$(command -v wget)" ]; then
+		sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+	else
+		echo "To install chezmoi, you must have curl or wget installed." >&2
+		exit 1
+	fi
 else
-  chezmoi=chezmoi
+	chezmoi=chezmoi
 fi
 
 # exec: replace current process with chezmoi init
